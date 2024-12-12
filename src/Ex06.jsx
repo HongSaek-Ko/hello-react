ca4bf7715727365b95c5b4ebaa45a42ec992015c
HongSaek-Ko <bh1152@naver.com>
2024-12-12 20:22:53 +0900

    1212 추가

--- src/Ex06.jsx
+++ src/Ex06.jsx
@@ -1,36 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const Ex06 = () => {
-  const [fruitName, setFruitName] = useState('')
-  const onChangeName = (e) => setFruitName(e.target.value)
+  const [fruitName, setFruitName] = useState('');
+  const onChangeName = (e) => setFruitName(e.target.value);
   const onClick = (e) => {
-    alert('추가 완료!') // id는 배열 길이+1로 설정  name은 입력(저장)했던 값
-    setFruits([...fruits, { id: ++fruits.length, name: fruitName }]) // 배열 객체를 가져오고, 배열에 새 객체({id: name}) 추가
+    alert('추가 완료!'); // id는 배열 길이+1로 설정  name은 입력(저장)했던 값
+    setFruits([...fruits, { id: ++fruits.length, name: fruitName }]); // 배열 객체를 가져오고, 배열에 새 객체({id: name}) 추가
     // const addFruits = fruits.concat({ id: ++fruits.length, name: fruitName}) // 기존 배열과 새로운 데이터를 연결 - 새로운 배열로 리턴
     // 지양: fruits.push... <<< 불변성의 원칙에 위배됨 (push 사용 시 기존 배열 수정됨)
-    setFruitName('') // 이벤트 완료 시점 - 입력한 결과값(전체 문자열) 빈 문자열로 초기화
-  }
+    setFruitName(''); // 이벤트 완료 시점 - 입력한 결과값(전체 문자열) 빈 문자열로 초기화
+  };
 
   // 클릭 이벤트를 엔터 시에도 발생
   const onKeyDown = (e) => {
     if (e.key === 'Enter') {
-      onClick()
+      onClick();
     }
-  }
+  };
   // 문 2. 아래 과일 목록을 화면에 출력하라.
   const [fruits, setFruits] = useState([
     { id: 1, name: '사과' },
     { id: 2, name: '딸기' },
     { id: 3, name: '수박' },
     { id: 4, name: '복숭아' },
-  ]) // 'fruit'은 단순 파라미터. const fruits 변수와 동일
+  ]); // 'fruit'은 단순 파라미터. const fruits 변수와 동일
   const fruitList = fruits.map((fruit) => (
     <li key={fruit.id}>
       {' '}
       {/* 고유 key값 부여*/}
       {fruit.id}: {fruit.name} {/* 목록(li) 구조 설정 (id: name) */}
     </li>
-  ))
+  ));
   // 문 3. 과일 이름을 입력받아 state에 추가하여, 새로 추가된 목록까지 화면에 출력하라.
   return (
     <div>
@@ -45,7 +45,7 @@ const Ex06 = () => {
       ></input>
       <button onClick={onClick}> 추가 </button>
     </div>
-  )
-}
+  );
+};
 
-export default Ex06
+export default Ex06;
--- src/Ex07.jsx
+++ src/Ex07.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { useState } from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 const Ex07 = () => {
   const [fruits, setFruits] = useState([
@@ -7,38 +7,38 @@ const Ex07 = () => {
     { id: 2, name: '딸기' },
     { id: 3, name: '수박' },
     { id: 4, name: '복숭아' },
-  ])
+  ]);
 
-  const [inputTxt, setInputTxt] = useState('')
-  const [nextId, setNextId] = useState(5)
+  const [inputTxt, setInputTxt] = useState('');
+  const [nextId, setNextId] = useState(5);
 
   const removeHandler = (id) => {
     // id값이 일치하는 객체를 제외한 리스트를 새로 받아오기
-    const filterFruits = fruits.filter((fruit) => fruit.id !== id)
+    const filterFruits = fruits.filter((fruit) => fruit.id !== id);
     // 배열 'fruits' 수정
-    setFruits(filterFruits) // state 변경 - component re-rendering
-  }
+    setFruits(filterFruits); // state 변경 - component re-rendering
+  };
 
   const fruitList = fruits.map((fruit) => (
     <li key={fruit.id} onDoubleClick={() => removeHandler(fruit.id)}>
       {fruit.id}: {fruit.name}
     </li>
-  ))
+  ));
 
   // 클릭 이벤트를 엔터 시에도 발생
   const onKeyDown = (e) => {
     if (e.key === 'Enter') {
-      clickAddHandler()
+      clickAddHandler();
     }
-  }
+  };
 
-  const changeInputHandler = (e) => setInputTxt(e.target.value)
+  const changeInputHandler = (e) => setInputTxt(e.target.value);
   const clickAddHandler = () => {
-    const copyFruits = fruits.concat({ id: nextId, name: inputTxt })
-    setFruits(copyFruits)
-    setNextId((prev) => prev + 1) // 기존(직전)값에 1 추가.
-    setInputTxt('')
-  }
+    const copyFruits = fruits.concat({ id: nextId, name: inputTxt });
+    setFruits(copyFruits);
+    setNextId((prev) => prev + 1); // 기존(직전)값에 1 추가.
+    setInputTxt('');
+  };
 
   return (
     <div>
@@ -53,7 +53,7 @@ const Ex07 = () => {
       ></input>
       <button onClick={clickAddHandler}> 추가 </button>
     </div>
-  )
-}
+  );
+};
 
-export default Ex07
+export default Ex07;
